b3797d2f25b77b85194bff9de98d88803e57cdaa
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-05-16 08:45:21 +0200

    first commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Ivan Trajanovski on 16.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
